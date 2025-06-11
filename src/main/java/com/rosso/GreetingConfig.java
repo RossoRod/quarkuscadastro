f4b83eef9806d3e8a1687de2b24ec122748dd691
quarkusio <no-reply@quarkus.io>
2025-06-11 00:52:15 +0000

    Initial commit

---
+++ src/main/java/com/rosso/GreetingConfig.java
@@ -0,0 +1,12 @@
+package com.rosso;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
