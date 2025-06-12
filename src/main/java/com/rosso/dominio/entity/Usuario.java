cb81de8356a2d7f876a274dd494b9702533ab81c
Rodrigo Rosso <rosso_rodrigo@hotmail.com>
2025-06-11 22:09:56 -0300

    crud funcionando

--- src/main/java/com/rosso/dominio/entity/Usuario.java
+++ src/main/java/com/rosso/dominio/entity/Usuario.java
@@ -1,9 +1,6 @@
 package com.rosso.dominio.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,22 +11,34 @@ import java.time.LocalDate;
 
 @Data
 @Entity
+@Table(name = "TB_USUARIO")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_USUARIO")
     private Integer idUsuario;
+
     @Size(min = 3, max = 100)
     @NotBlank
+    @Column(name = "NOME")
     private String nome;
+
     @Size(min = 3, max = 100)
     @NotBlank
+    @Column(name = "LOGIN_USER")
     private String loginUser;
+
     @Size(min = 3, max = 100)
     @NotBlank
+    @Column(name = "SENHA")
     private String senha;
+
     @Email
     @NotBlank
+    @Column(name = "EMAIL")
     private String email;
-    @NotNull
-    private LocalDate dataCadastro;
+
+//    @NotNull
+//    @Column(name = "")
+//    private LocalDate dataCadastro;
 }
